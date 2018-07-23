12bd271cff41294f45f6e4a781222d9935809d11
Maximus <maximgenov@abv.bg>
2018-07-24 00:38:34 +0300

    Fixed Products Router methods.

--- modules/products/router.js
+++ modules/products/router.js
@@ -3,7 +3,7 @@ const productsController = require('./controllers/index')
 
 module.exports = router
   .post('/create', productsController.create)
-  .post('/remove/:id', productsController.remove)
+  .delete('/remove/:id', productsController.remove)
   .get('/get/:id', productsController.get)
   .get('/all', productsController.getAll)
-  .post('/update/:id', productsController.update)
+  .put('/update/:id', productsController.update)
