a7d75b3625d2ee3f8d53b4fd51dd605f4c0ce32a
Maximus <maximgenov@abv.bg>
2018-08-14 21:08:46 +0300

    Updated  CORS

--- utils/cors.js
+++ utils/cors.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization')
-  res.header('Access-Control-Allow-Methods', 'DELETE')
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST')
 
   next()
 }
